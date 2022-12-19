16c35de0c861359a5fa7372815d85d1d61ea8e79
Kiefer Cover <kiefer_cover@hotmail.com>
2022-12-18 19:18:19 -0700

    start using material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import '../styles/Clock.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { CssBaseline } from '@mui/material';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (<main>
+    <CssBaseline />
+    <Head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" key="material-meta"/>
+    </Head>
+    <Component {...pageProps} />
+    </main>);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,65 @@
 import { useEffect, useState } from "react";
-import Clock from 'react-clock';
-import 'react-clock/dist/Clock.css';
-import styles from '../styles/Clock.module.css';
+import Clock from "react-clock";
+import styles from "../styles/Clock.module.css";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import styled from "@emotion/styled";
+import InputLabel from "@mui/material/InputLabel";
 
-export default function App() {
- const [value, setValue] = useState(new Date());
+interface PropsType {
+  initialSize: number;
+}
+
+const StyledMain = styled.main`
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: url(/Butte_sunrise.jpg);
+    background-size: auto;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const StyledFormControl = styled(FormControl)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+ `
+
+const StyledSelectLabel = styled(InputLabel)`
+  color: #fff;
+`
+
+const StyledSelect = styled(Select)`
+  background-color: #1976d2;
+  display: block;
+
+`
+
+const StyledBeginButton = styled(Button)`
+display: block;
+`
+
+export default function App({ initialSize = 150 }: PropsType) {
+  const [value, setValue] = useState(new Date());
+  const [size, setSize] = useState(initialSize);
+  // overall time in seconds
+  const [lengthTime, setLengthTime] = useState(5);
+  const markSize = 15;
+
+  const handleLengthChange = (event: SelectChangeEvent<unknown>) =>
+    setLengthTime(
+      typeof event.target.value === "string"
+        ? parseInt(event.target.value)
+        : event.target.value as number
+    );
 
   useEffect(() => {
     const interval = setInterval(() => setValue(new Date()), 1000);
@@ -14,17 +69,50 @@ export default function App() {
     };
   }, []);
 
-  const smallestAspect = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
-  const clockSize = Math.floor(smallestAspect * 0.75);
+  // const handleResize = () => {
+  //   const smallestAspect =
+  //     window.innerHeight < window.innerWidth
+  //       ? window.innerHeight
+  //       : window.innerWidth;
+  //   setSize(Math.floor(smallestAspect * 0.75));
+  // };
+
+  // useEffect(() => {
+  //   handleResize();
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
   return (
-    <main className={styles.main}>
-      <Clock 
-      className={styles.clock} 
-      value={value} 
-      renderMinuteMarks={false}
-      size={clockSize}
+    <StyledMain>
+      <Clock
+        className={styles.clock}
+        value={value}
+        hourMarksWidth={markSize}
+        renderMinuteMarks={false}
+        renderMinuteHand={false}
+        secondHandWidth={markSize}
+        size={size}
       />
-    </main>
+      <div style={{ width: '100%', height: '100%' }}>
+      <StyledFormControl variant="filled">
+      <StyledSelectLabel id="length-select-label">Length</StyledSelectLabel>
+      <StyledSelect
+        id="length-select"
+        labelId="length-select-label"
+        variant="filled"
+        value={lengthTime}
+        defaultValue={3}
+        label="Length"
+        onChange={handleLengthChange}
+      >
+        <MenuItem value={3}>3 seconds</MenuItem>
+      </StyledSelect>
+      <StyledBeginButton variant="contained">Begin</StyledBeginButton>
+      </StyledFormControl>
+  </div>
+    </StyledMain>
   );
 }
